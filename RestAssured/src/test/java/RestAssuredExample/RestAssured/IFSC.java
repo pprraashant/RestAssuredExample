82b224977de131fc2f0acdf298d3cb29dee29d43
Prashant Pawar (99073) <prashant.pawar1@amdocs.com>
2019-04-13 10:02:54 +0530

    new changes

--- RestAssured/src/test/java/RestAssuredExample/RestAssured/IFSC.java
+++ RestAssured/src/test/java/RestAssuredExample/RestAssured/IFSC.java
@@ -4,31 +4,38 @@ import static io.restassured.RestAssured.*;
 import static io.restassured.matcher.RestAssuredMatchers.*;
 import static org.hamcrest.Matchers.*;
 
+import org.junit.Assert;
 import org.junit.Test;
 
+import io.restassured.RestAssured;
 import io.restassured.http.Method;
 import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 public class IFSC {
 	
 	@Test
-	public boolean validateWeather()
+	public void validateWeather()
 	{
 		baseURI="https://ifsc.razorpay.com";
-		 RequestSpecification httpRequest= given();
+		 RequestSpecification httpRequest= given().proxy("135.28.13.11", 8080);
 		 
-		 Response response=httpRequest.request("/CITI0000005");
+		 Response response=httpRequest.request(Method.GET,"/CITI0000005");
 		
 		 String responseBody=response.getBody().asString();
 		 System.out.println("The response is :"+responseBody);
-		return true;
+		// ValidatableResponse re=given().get("/Pune").then().body("City", equalTo("Pune"));
+		 
+		// Assert.assertTrue(given().proxy("135.28.13.11", DEFAULT_PORT).get("/CITI0000005").then().body("CITY", equalTo("PUNE")).equals(true));
+		//return true;
 	}
 	
 	public static void main(String [] args)
 	{
+//		RestAssured.proxy=host("135.28.13.11").
 		baseURI="https://ifsc.razorpay.com";
-		 RequestSpecification httpRequest= given();
+		 RequestSpecification httpRequest= given().proxy("135.28.13.11", DEFAULT_PORT);
 		 
 		 Response response=httpRequest.request(Method.GET,"/CITI0000005");
 		
